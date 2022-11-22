295a38b740e25fb8acf9ea115d987793ceeb4c09
Dionel22 <diondiaz293@gmail.com>
2022-11-21 21:13:43 -0300

    first commit

---
+++ 01-IntroToCS/homework/homework.js
@@ -0,0 +1,24 @@
+'use strict';
+// No usar parseInt para String 
+function BinarioADecimal(num) {
+   var decimal = num.split("").reverse();
+   var total = 0;
+   for (let i = 0; i < decimal.length; i++) {
+      total = total + decimal[i] * 2 ** i;
+   }
+   return total;
+}
+
+function DecimalABinario(num) {
+   var binario = '';
+   while (num >= 1) {
+      binario = num % 2 + binario;
+      num = Math.floor(num / 2);
+   }
+   return binario;
+}
+
+module.exports = {
+   BinarioADecimal,
+   DecimalABinario,
+};
