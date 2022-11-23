48f77f8725d5a5cef4aad2d2ae19ee7ff7bfbc3c
Dionel22 <diondiaz293@gmail.com>
2022-11-23 17:36:46 -0300

    segundo dia

--- 02-JavaScriptAvanzado-I/homework/ejemlo.js
+++ 02-JavaScriptAvanzado-I/homework/ejemlo.js
@@ -1,144 +1,144 @@
-x = 1;// no tiene hoistyn tira error / hoistin funciona solo con var
-var a = 5;//
-var b = 10;//
-var c = function (a, b, c) {
-    //
-    //var a = 8; var b = 9; var c = 10
-   var x = 10;//
-   console.log(x);//10
-   console.log(a);//8
-   var f = function (a, b, c) {
-      b = a;// b= 8
-      console.log(b);// 8
-      b = c;// b = 10
-      var x = 5;
-   };
-   f(a, b, c);
-   console.log(b);//9
-};
-c(8, 9, 10);
-console.log(b);//10
-console.log(x);//1
+// x = 1;// no tiene hoistyn tira error / hoistin funciona solo con var
+// var a = 5;//
+// var b = 10;//
+// var c = function (a, b, c) {// la espresa  // contexto
+//     //
+//     //var a = 8; var b = 9; var c = 10
+//    var x = 10;//
+//    console.log(x);//10
+//    console.log(a);//8
+//    var f = function (a, b, c) {// la declara // contexto
+//       b = a;// b= 8
+//       console.log(b);// 8
+//       b = c;// b = 10
+//       var x = 5;
+//    };
+//    f(a, b, c);
+//    console.log(b);//9
+// };
+// c(8, 9, 10);
+// console.log(b);//10
+// console.log(x);//1
 
-//-----------------
+// //-----------------
 
-// var bar; Hoisting
-console.log(bar);// undefined
-console.log(baz);// 
-foo();
-function foo() {
-   console.log('Hola!');
-}
-// "hola"
-var bar = 1;
-baz = 2; // no fue declarada con var
+// // var bar; Hoisting
+// console.log(bar);// undefined
+// console.log(baz);// error -> baz no existe
+// foo();
+// function foo() {// se eleva
+//    console.log('Hola!');
+// }
+// // "hola"
+// var bar = 1;
+// baz = 2; // no fue declarada con var
 
-//-----------------
-
-
-var instructor = 'Tony';//undefiend
-if (true) {
-   var instructor = 'Franco';
-}
-console.log(instructor);//"franco"
-
-//-----------------------------
-
-var instructor = 'Tony';
-console.log(instructor);// "tony"
-(function () {
-   if (true) {
-      var instructor = 'Franco';
-      console.log(instructor);
-   }
-})();// IIFE LA CREO Y LA EJECUTO //
-console.log(instructor);// "tony"
-
-//----------------------------
-
-var instructor = 'Tony';
-let pm = 'Franco';
-if (true) {
-   var instructor = 'The Flash';
-   let pm = 'Reverse Flash';
-   console.log(instructor);// "The flash"
-   console.log(pm);//"Reverse Flash"
-}
-console.log(instructor);//"The Flash"
-console.log(pm);//Franco
-
-//--------------------------
+// //-----------------
 
 
-console.log(6 / "3");// 2
-// / - *
-console.log("2" * "3");//6
-console.log(4 + 5 + "px");// 9 + "px" = "9px"
-console.log("$" + 4 + 5);//"$4" + 5 = "$45"
-console.log("4" - 2);//2
-console.log("4px" - 2);// NaN
-console.log(7 / 0);// infinity
-console.log({}[0])// undefined
-console.log(parseInt("09"));//9
-console.log(5 && 2);// 2
-console.log(2 && 5);// 5
-console.log(5 || 0);// 5
-console.log(0 || 5);// 5
-console.log([3]+[3]-[10]);// 23 concadena lo combierte en estring de lo desconvierte
-console.log(3>2>1);// false
-console.log([] == ![]);// true
-        //  [] == false
-        //  "" ==  0
-        //  0  ==  0  true
+// var instructor = 'Tony';//undefiend
+// if (true) { // if es un bloque
+//    var instructor = 'Franco'; 
+// }
+// console.log(instructor);//"franco"
 
-//-----------------------
+// //-----------------------------
 
-function test() {
-    //var a;
-    console.log(a);// undefiend
-    console.log(foo());// 2
+// var instructor = 'Tony';
+// console.log(instructor);// "tony"
+// (function () {
+//    if (true) {
+//       var instructor = 'Franco';
+//       console.log(instructor);
+//    }
+// })();// IIFE LA CREO Y LA EJECUTO //
+// console.log(instructor);// "tony"
+
+// //----------------------------
+
+// var instructor = 'Tony';
+// let pm = 'Franco';
+// if (true) {
+//    var instructor = 'The Flash'; // lo pisa
+//    let pm = 'Reverse Flash';
+//    console.log(instructor);// "The flash"
+//    console.log(pm);//"Reverse Flash"
+// }
+// console.log(instructor);//"The Flash"
+// console.log(pm);//Franco
+
+// //--------------------------
+
+
+// console.log(6 / "3");// 2
+// // / - *
+// console.log("2" * "3");//6
+// console.log(4 + 5 + "px");// 9 + "px" = "9px"
+// console.log("$" + 4 + 5);//"$4" + 5 = "$45"
+// console.log("4" - 2);//2
+// console.log("4px" - 2);// NaN
+// console.log(7 / 0);// infinity
+// console.log({}[0])// undefined
+// console.log(parseInt("09"));//9
+// console.log(5 && 2);// 2
+// console.log(2 && 5);// 5
+// console.log(5 || 0);// 5
+// console.log(0 || 5);// 5
+// console.log([3]+[3]-[10]);// 23 concadena lo combierte en estring de lo desconvierte
+// console.log(3>2>1);// false 
+// console.log([] == ![]);// true
+//         //  [] == false
+//         //  "" ==  0
+//         //  0  ==  0  true
+
+// //-----------------------
+
+// function test() {
+//     //var a;
+//     console.log(a);// undefiend
+//     console.log(foo());// 2
  
-    var a = 1;
-    function foo() {
-       return 2;
-    }
- }
+//     var a = 1;
+//     function foo() {
+//        return 2;
+//     }
+//  }
  
- test();
+//  test();
 
- //---------
+//  //---------
 
- var snack = 'Meow Mix';
+//  var snack = 'Meow Mix';
 
-function getFood(food) {
-    //var snack; no entra al if y da undefiend
-   if (food) {
-      var snack = 'Friskies';
-      return snack;
-   }
-   return snack;
-}
+// function getFood(food) {
+//     //var snack; no entra al if y da undefiend
+//    if (food) {
+//       var snack = 'Friskies';
+//       return snack;
+//    }
+//    return snack;// undefiend
+// }
 
-getFood(false);
+// getFood(false);
 
-//------------
+// //------------
 
-var fullname = 'Juan Perez';
-var obj = {
-   fullname: 'Natalia Nerea',
-   prop: {
-      fullname: 'Aurelio De Rosa',
-      getFullname: function () {
-         return this.fullname;// objeto this ase relacion con prop
-      },
-   },
-};
+// var fullname = 'Juan Perez';
+// var obj = {
+//    fullname: 'Natalia Nerea',
+//    prop: {
+//       fullname: 'Aurelio De Rosa',
+//       getFullname: function () {
+//          return this.fullname;// objeto this ase relacion con prop
+//       },
+//    },
+// };
 
-console.log(obj.prop.getFullname());// "Aurelio de rosa"
+// console.log(obj.prop.getFullname());// "Aurelio de rosa"
 
-var test = obj.prop.getFullname;// this --> windows
+// var test = obj.prop.getFullname;// this --> windows
 
-console.log(test());// undefiend
+// console.log(test());// undefiend
 
 //-----------------
 
