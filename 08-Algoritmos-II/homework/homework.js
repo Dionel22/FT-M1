ed7a728f1c884097ce4cbdeff04124d29012723b
Dionel22 <diondiaz293@gmail.com>
2022-11-30 22:05:53 -0300

    octavo  dia

--- 08-Algoritmos-II/homework/homework.js
+++ 08-Algoritmos-II/homework/homework.js
@@ -6,7 +6,22 @@ function quickSort(array) {
   // el array recibido como parámetro
   // Devolver el array ordenado resultante
   // Tu código:
-
+  let arr = []
+  if(array.length < 1) {
+    return []
+  }
+    var left = []
+    var right = []
+    var pivot = array[0];
+  for (let i = 1; i < array.length; i++) {
+      if(array[i] < pivot){
+        left.push(array[i]);
+      }else{
+        right.push(array[i]);  
+      } 
+  } 
+  return arr.concat(quickSort(left), pivot, quickSort(right));
+ 
 }
 
 function mergeSort(array) {
@@ -16,6 +31,27 @@ function mergeSort(array) {
   // Tu código:
 
   //okey probemos ahora
+  if(array.length <= 1){
+    return array;
+  }
+  var medio = Math.floor(array.length / 2);
+  var left = array.slice(0, medio)
+  var right = array.slice(medio)
+
+  return merge(mergeSort(left), mergeSort(right) )
+}
+
+function merge(left, right) {
+  let arr = [];
+  while (left.length && right.length) {
+    if(left[0] < right[0]){
+      arr.push(left.shift());
+    }else{
+      arr.push(right.shift());
+    }
+    
+  }
+  return arr.concat(left.slice()).concat(right.slice());
 }
 
 // No modificar nada debajo de esta línea
